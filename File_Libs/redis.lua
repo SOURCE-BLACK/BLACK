35a06e4361afe666ed61a192089adafc994c83ac
DEV'' hassan muaed <60893857+SOURCE-BLACK@users.noreply.github.com>
2020-08-05 23:15:29 +0300

    Add files via upload

--- File_Libs/redis.lua
+++ File_Libs/redis.lua
@@ -264,8 +264,10 @@ function network.read(client, len)
     if not err then return line else
 	local TokenBot = io.open('./inc/Token.txt', "r"):read('*a')
 print("\27[34m"..[[
+
 >> Best Source in Telegram
 >> Features fast and powerful
+
  /$$$$$$$$        /$$                 /$$                
 |__  $$__/       | $$                | $$                
    | $$  /$$$$$$$| $$$$$$$   /$$$$$$ | $$   /$$  /$$$$$$ 
@@ -789,8 +791,10 @@ local function connect_tcp(socket, parameters)
     local ok, err = socket:connect(host, port)
     if not ok then
 	print("\27[34m"..[[
+
 >> Best Source in Telegram
 >> Features fast and powerful
+
  /$$$$$$$$        /$$                 /$$                
 |__  $$__/       | $$                | $$                
    | $$  /$$$$$$$| $$$$$$$   /$$$$$$ | $$   /$$  /$$$$$$ 
